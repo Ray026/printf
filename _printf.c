66372712c8213719a2055936ba2f097aa29d493e
Simiyuraymond <99405275+Simiyuraymond@users.noreply.github.com>
2022-04-19 21:58:35 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -75,16 +75,12 @@ int _printf(const char *format, ...)
         buffer_t *output;
         va_list args;
         int ret;
-
         if (format == NULL)
                 return (-1);
         output = init_buffer();
         if (output == NULL)
                 return (-1);
-
         va_start(args, format);
-
         ret = run_printf(format, args, output);
-
         return (ret);
 }
